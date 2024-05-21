49d2df44b1ea1ac02ab2687e40b96c7c58979c86
JuhyeokC <bos2ablues@gmail.com>
2024-05-21 15:21:23 +0900

    :art: restore image path

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
+import logo from './images/logo/logo.png';
 import Buttons from './Buttons';
 
 export default function Header({ gameManager }) {
-	const imagePath = process.env.PUBLIC_URL + `/images/logo/logo.png`;
 	return (
 		<header>
-			<img className='App-logo' alt='주사위게임 로고' src={imagePath} />
+			<img className='App-logo' alt='주사위게임 로고' src={logo} />
 			<h1 className='App-title'>주사위게임</h1>
 			<Buttons gameManager={gameManager} />
 		</header>
